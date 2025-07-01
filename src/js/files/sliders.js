dae1e48118f8773451b0a93a19d4d3bab14c18ce
pauchok-sk <pauchoooooook@gmail.com>
2025-07-02 00:51:39 +0300

    hero

--- src/js/files/sliders.js
+++ src/js/files/sliders.js
@@ -21,4 +21,55 @@ export default function sliders() {
       },
     });
   }
+
+  const heroReklamSlider = document.querySelector(".hero__reklam-slider");
+
+  if (heroReklamSlider) {
+    const swiper = new Swiper(heroReklamSlider, {
+      speed: 800,
+      spaceBetween: 15,
+      autoplay: {
+        delay: 3500,
+      },
+      navigation: {
+        prevEl: ".hero__reklam-slider .slider-btn._prev",
+        nextEl: ".hero__reklam-slider .slider-btn._next",
+      },
+      pagination: {
+        el: ".hero__reklam .slider-pagination",
+        clickable: true,
+      },
+    });
+  }
+
+  const heroHitSlider = document.querySelector(".hero__hit-slider");
+
+  if (heroHitSlider) {
+    const swiper = new Swiper(heroHitSlider, {
+      speed: 800,
+      spaceBetween: 10,
+      slidesPerView: "auto",
+      autoplay: {
+        delay: 2500,
+      },
+      navigation: {
+        prevEl: ".hero__hit .slider-btn._prev",
+        nextEl: ".hero__hit .slider-btn._next",
+      },
+      pagination: {
+        el: ".hero__hit .slider-pagination",
+        clickable: true,
+      },
+      breakpoints: {
+        1200: {
+          spaceBetween: 15,
+          slidesPerView: 1,
+        },
+        767: {
+          spaceBetween: 15,
+          slidesPerView: "auto",
+        },
+      },
+    });
+  }
 }
