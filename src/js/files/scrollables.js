18f86beeb800cfe6dfdf66445007336392908896
pauchok-sk <pauchoooooook@gmail.com>
2025-07-14 12:26:10 +0300

    thanks-reviews page

--- src/js/files/scrollables.js
+++ src/js/files/scrollables.js
@@ -4,4 +4,6 @@ export default function scrollables() {
   if (window.matchMedia("(max-width: 767px)").matches) {
     new Scrollable(".s-history__nav");
   }
+
+  new Scrollable(".s-archive-reviews__nav");
 }
